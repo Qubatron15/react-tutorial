96581c79d63892faa3b8070794c8ecf291176c13
Kuba <qubatron15@gmail.com>
2024-10-17 18:39:22 +0200

    adding the goals to state with button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
 import CourseGoal from "./components/CourseGoal";
 import goalsImg from './assets/goals.jpg';
 import Header from "./components/Header";
+import { useState } from "react";
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  id: number;
+}
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal() {
+    setGoals(prevGoals => {
+      const newGoal: CourseGoal = {
+        title: 'testowy cel',
+        description: 'jakis opis testowego celu',
+        id: Math.random(),
+      }
+
+      return [...prevGoals, newGoal];
+    });
+  }
+
   return (
     <main>
       <Header image={{src: goalsImg, alt: 'App logo'}}>
         <h1>Your Course Goal</h1>
       </Header>
-      <CourseGoal title="Nauka Reacka i TS">
-        <p>Nauka reactaaaaaaa XD</p>
-      </CourseGoal>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <ul>
+        {
+          goals.map((goal) => (
+            <li key={goal.id}>
+              <CourseGoal title={goal.title}>
+                <p>{goal.description}</p>
+              </CourseGoal>
+            </li>
+          ))
+        }
+      </ul>
     </main>
   )
 }
